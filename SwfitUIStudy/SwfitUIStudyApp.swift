bdf708ff04896a9762852db912d0f22893bb5c14
jin <pearlisrich@naver.com>
2022-04-25 11:04:58 +0900

    Initial Commit

---
+++ SwfitUIStudy/SwfitUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUIStudyApp.swift
+//  SwfitUIStudy
+//
+//  Created by jin on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
