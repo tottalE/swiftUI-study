438da0fe97ef543f43ccab4f8a1d54ae5f8471ec
jin <pearlisrich@naver.com>
2022-05-04 19:03:40 +0900

    extracted function

---
+++ SwfitUIStudy/_19ExtractedFunction.swift
@@ -0,0 +1,47 @@
+//
+//  _19ExtractedFunction.swift
+//  SwfitUIStudy
+//
+//  Created by jin on 5/4/22.
+//
+
+import SwiftUI
+
+struct _19ExtractedFunction: View {
+    
+    @State var backGroundColor: Color = Color.pink
+    
+    var body: some View {
+        ZStack {
+            // background
+            backGroundColor.edgesIgnoringSafeArea(.all)
+            
+            //content
+            VStack {
+                Text("Title")
+                    .font(.largeTitle)
+                
+                Button(action: {
+                    buttonPressed()
+                }, label: {
+                    Text("PRESS ME")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(10)
+                })
+            }
+        }
+    }
+    
+    func buttonPressed() {
+        backGroundColor = .yellow
+    }
+}
+
+struct _19ExtractedFunction_Previews: PreviewProvider {
+    static var previews: some View {
+        _19ExtractedFunction()
+    }
+}
