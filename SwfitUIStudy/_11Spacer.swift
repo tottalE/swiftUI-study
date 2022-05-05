a546c6ef008d80cde46a09c699658827db434d3a
jin <pearlisrich@naver.com>
2022-05-05 20:17:37 +0900

    padding

--- SwfitUIStudy/_11Spacer.swift
+++ SwfitUIStudy/_11Spacer.swift
@@ -10,18 +10,60 @@ import SwiftUI
 struct _1Spacer: View {
     var body: some View {
         HStack(spacing: 0) {
-            Spacer()
+            Spacer(minLength: nil)
                 .frame(height: 10)
                 .background(Color.orange)
             
             Rectangle()
                 .frame(width: 50, height: 50)
             
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
+            
             Rectangle()
                 .fill(Color.red)
                 .frame(width: 50, height: 50)
+            
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 50, height: 50)
+            
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
         }
-        .background(Color.blue)
+        .background(Color.yellow)
+//        .padding(.horizontal, 200)
+        .background(Color.green)
+        
+        VStack {
+            HStack(spacing: 0) {
+                Image(systemName: "xmark")
+                Spacer()
+                    .frame(height: 10)
+//                    .background(Color.orange)
+                Image(systemName: "gear")
+            }
+//            .frame(height: 10)
+            .font(.title)
+//            .background(Color.yellow)
+            .padding(.horizontal)
+//            .background(Color.blue)
+            
+            Spacer()
+                .frame(width: 10)
+//                .background(Color.orange)
+            
+            Rectangle()
+                .frame(height: 55)
+        }
+//        .background(Color.yellow)
+        
     }
 }
 
