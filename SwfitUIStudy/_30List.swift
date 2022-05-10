4cfb515fc20fb46d984e632ad7ebc455cab810eb
jin <pearlisrich@naver.com>
2022-05-10 09:22:18 +0900

    List

--- SwfitUIStudy/_30List.swift
+++ SwfitUIStudy/_30List.swift
@@ -12,6 +12,9 @@ struct _30List: View {
     @State var fruits: [String] = [
         "apple", "orange", "banana", "peach"
     ]
+    @State var veggies: [String] = [
+        "tomato", "potato", "carrot"
+    ]
     
     var body: some View {
         NavigationView {
@@ -19,23 +22,55 @@ struct _30List: View {
                 Section {
                     ForEach(fruits, id: \.self) { fruit in
                         Text(fruit.capitalized)
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .padding(.vertical)
                     }
                     .onDelete(perform: delete(indextSet:))
-                    .onMove { indices, newOffset in
-                        fruits
-                    }
+//                    .onMove { indices, newOffset in
+//                        move(indices: indices, newOffset: newOffset)
+//                    }
+                    .onMove(perform: move)
+                    .listRowBackground(Color.pink)
                 } header: {
-                    Text("FRUITS")
+                    HStack {
+                        Text("FRUITS")
+                        Image(systemName: "flame.fill")
+                    }
+                    .font(.headline)
+                    .foregroundColor(Color.orange)
                 }
+                
+                Section(content: {
+                    ForEach(veggies, id:\.self) { veggies in
+                        Text(veggies.capitalized)
+                    }
+                }, header: {Text("VEGGIES")})
             }
+            .accentColor(.purple)
+            .listStyle(SidebarListStyle())
             .navigationTitle("Grocery List")
-            .navigationBarItems(leading: EditButton())
+            .navigationBarItems(leading: EditButton(), trailing: addButton)
         }
-        
+        .accentColor(.red)
+    }
+    
+    var addButton: some View {
+        Button("Add", action: {
+            add()
+        })
     }
     
     func delete(indextSet: IndexSet) {
         fruits.remove(atOffsets: indextSet)
+    }
+    
+    func move(indices: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func add() {
+        fruits.append("Coconut")
     }
 }
 
