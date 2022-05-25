7cff14ca9aa52f6dee65d19c0edabeb5dfe7cde6
jin <pearlisrich@naver.com>
2022-05-25 23:00:13 +0900

    Model(struct)

---
+++ SwfitUIStudy/_50Model.swift
@@ -0,0 +1,69 @@
+//
+//  _50Model.swift
+//  SwfitUIStudy
+//
+//  Created by jin on 5/25/22.
+//
+
+import SwiftUI
+
+struct UserModel: Identifiable {
+    let id: String = UUID().uuidString
+    let displayName: String
+    let userName: String
+    let followerCount: Int
+    let isVerified: Bool
+}
+
+struct _50Model: View {
+    
+    @State var users: [UserModel] = [
+        UserModel(displayName: "Nick", userName: "nick123", followerCount: 100, isVerified: true),
+        UserModel(displayName: "Emily", userName: "emily123", followerCount: 55, isVerified: false),
+        UserModel(displayName: "Random", userName: "rdm123", followerCount: 355, isVerified: true),
+        UserModel(displayName: "Chris", userName: "cris123", followerCount: 88, isVerified: false)
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(users) { user in
+                    HStack(spacing: 15.0) {
+                        Circle()
+                            .frame(width: 35, height: 35)
+                        VStack(alignment: .leading) {
+                            Text(user.displayName)
+                                .font(.headline)
+                            Text("@\(user.userName)")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
+                        Spacer()
+                        
+                        if user.isVerified {
+                            Image(systemName: "checkmark.seal.fill")
+                                .foregroundColor(.blue)
+                        }
+                        
+                        VStack {
+                            Text("\(user.followerCount)")
+                                .font(.headline)
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
+                    }
+                    .padding(.vertical, 10)
+                }
+            }
+            .listStyle(InsetListStyle())
+            .navigationTitle("Users")
+        }
+    }
+}
+
+struct _50Model_Previews: PreviewProvider {
+    static var previews: some View {
+        _50Model()
+    }
+}
