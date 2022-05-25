8f3eede607fb29e18abc31760b3ca6c4ef22085e
jin <pearlisrich@naver.com>
2022-05-25 22:39:59 +0900

    on tab gesture

---
+++ SwfitUIStudy/_49TabGesture.swift
@@ -0,0 +1,56 @@
+//
+//  _49TabGesture.swift
+//  SwfitUIStudy
+//
+//  Created by jin on 5/25/22.
+//
+
+import SwiftUI
+
+struct _49TabGesture: View {
+    
+    @State var isSelected: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height: 200)
+                .foregroundColor(isSelected ? Color.green : Color.red)
+            
+            Button(action: {
+                isSelected.toggle()
+            }, label: {
+                Text("Button")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(25)
+            })
+            
+            Text("TAP Gesture")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(25)
+//                .onTapGesture {
+//                    isSelected.toggle()
+//                }
+                .onTapGesture(count: 2, perform: {
+                    isSelected.toggle()
+                })
+            
+            Spacer()
+        }
+        .padding(40)
+    }
+}
+
+struct _49TabGesture_Previews: PreviewProvider {
+    static var previews: some View {
+        _49TabGesture()
+    }
+}
