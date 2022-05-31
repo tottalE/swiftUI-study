61b7b56985888c94f22def1ba29c0e9e5c61edbf
jin <pearlisrich@naver.com>
2022-05-31 09:22:35 +0900

    app storage

---
+++ SwfitUIStudy/_53AppStorage.swift
@@ -0,0 +1,41 @@
+//
+//  _53AppStorage.swift
+//  SwfitUIStudy
+//
+//  Created by jin on 5/27/22.
+//
+
+import SwiftUI
+
+struct _53AppStorage: View {
+    
+//    @State var currentUserName: String?
+    @AppStorage("name") var currentUserName: String?
+    
+    var body: some View {
+        
+        VStack(spacing: 20) {
+            Text(currentUserName ?? "Add name here")
+            
+            if let name = currentUserName {
+                Text(name)
+            }
+            
+            
+            Button("Save".uppercased()) {
+                let name = "Emily"
+                currentUserName = name
+//                UserDefaults.standard.set(name, forKey: "name")
+            }
+        }
+        .onAppear {
+//            currentUserName = UserDefaults.standard.string(forKey: "name")
+        }
+    }
+}
+
+struct _53AppStorage_Previews: PreviewProvider {
+    static var previews: some View {
+        _53AppStorage()
+    }
+}
